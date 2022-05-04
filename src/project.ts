c3135d50305f828794a779b34d7718d84ad23faa
fabian.bergstrom <fabian.bergstrom@interactivesolutions.se>
2022-05-04 18:31:33 +0200

    add allocation code

---
+++ src/project.ts
@@ -0,0 +1,52 @@
+import { djb2 } from "./hash";
+
+export type ProjectState = "paused" | "active";
+
+export type SymplifyConfig = {
+    /** in seconds from epoch */
+    updated: number;
+    projects: ProjectConfig[];
+};
+
+export type ProjectConfig = {
+    id: number;
+    name: string;
+    variations: VariationConfig[];
+    state: ProjectState;
+};
+
+export type VariationConfig = {
+    id: number;
+    name: string;
+    weight: number;
+    state: ProjectState;
+};
+
+export function findVariationForVisitor(
+    project: ProjectConfig,
+    visitorID: string
+): VariationConfig | null {
+    if (!visitorID) {
+        return project.variations[0];
+    }
+
+    let totalWeight = 0;
+    for (const variation of project.variations) {
+        totalWeight += variation.weight;
+    }
+
+    const hashKey = `${visitorID}:${project.id}`;
+    let hash = djb2(hashKey);
+    hash /= 4_294_967_295; // unsigned max
+    hash *= totalWeight; // fit window
+
+    let pointer = 0;
+    for (const variation of project.variations) {
+        pointer += variation.weight;
+        if (hash <= pointer) {
+            return variation;
+        }
+    }
+
+    return null;
+}
---
+++ test/allocation.test.ts
@@ -0,0 +1,70 @@
+import { findVariationForVisitor, ProjectConfig } from "../src/project";
+
+const TEST_PROJECT: ProjectConfig = {
+    id: 4711,
+    name: "discount",
+    state: "active",
+    variations: [
+        {
+            id: 42,
+            name: "original",
+            state: "active",
+            weight: 2,
+        },
+        {
+            id: 1337,
+            name: "massive",
+            state: "active",
+            weight: 1,
+        },
+    ],
+};
+
+describe("Variation allocation", () => {
+    /** a nonsense variation to help handling nulls in the test code */
+    const sentinelVariation = { id: "nope" };
+
+    test("is weighted", () => {
+        const variation = findVariationForVisitor(TEST_PROJECT, "foobar") || sentinelVariation;
+        expect(variation.id).toBe(42);
+    });
+
+    test("is distributed", () => {
+        const variation = findVariationForVisitor(TEST_PROJECT, "Fabian") || sentinelVariation;
+        expect(variation.id).toBe(1337);
+    });
+
+    test("returns the original variation for empty visitor ID", () => {
+        const originalID = Math.ceil(Math.random() * 1000);
+        const variationID = 1000 + Math.ceil(Math.random() * 1000);
+        const testProject: ProjectConfig = {
+            id: 10000,
+            name: "test project",
+            state: "active",
+            variations: [
+                {
+                    id: originalID,
+                    name: "Original",
+                    state: "active",
+                    weight: 1,
+                },
+                {
+                    id: variationID,
+                    name: "Variation",
+                    state: "active",
+                    weight: 1000,
+                },
+            ],
+        };
+
+        const variation = findVariationForVisitor(testProject, "") || sentinelVariation;
+        expect(variation.id).toBe(originalID);
+    });
+
+    test("handles long visitor IDs", () => {
+        // eslint-disable-next-line prettier/prettier
+        const visid = "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890";
+        const variation = findVariationForVisitor(TEST_PROJECT, visid) || sentinelVariation;
+        expect(variation.id).toBe(1337);
+    });
+});
