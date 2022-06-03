7145b31b390f66cdf3cb1a1a420fe680627b0913
fabian.bergstrom <fabian.bergstrom@interactivesolutions.se>
2022-06-03 11:51:59 +0200

    add custom attribute getters, runtime type checks

--- src/audience.ts
+++ src/audience.ts
@@ -94,7 +94,7 @@ export class Audience {
                 }
                 evaledArgs.push(a);
             }
-            return prim.apply(prim, evaledArgs);
+            return prim(evaledArgs, env);
         } catch {
             return { message: `error applying primitive ${head}` };
         }
@@ -156,13 +156,14 @@ function numberFun(a: Atom, b: Atom, op: (x: number, y: number) => Atom): Audien
 // eslint-disable-next-line @typescript-eslint/ban-types
 const primitives: Record<string, Function> = {
     // boolean operations
-    not: (arg: Atom) => {
+    not: (args: Atom[]) => {
+        const [arg] = args;
         if (typeof arg == "boolean") {
             return !arg;
         }
         return { message: `${arg} is not a boolean` };
     },
-    all: (...args: List) => {
+    all: (args: Atom[]) => {
         for (const arg of args) {
             if (!(typeof arg == "boolean")) {
                 return { message: `${arg} is not a boolean` };
@@ -173,7 +174,7 @@ const primitives: Record<string, Function> = {
         }
         return true;
     },
-    any: (...args: List) => {
+    any: (args: Atom[]) => {
         for (const arg of args) {
             if (!(typeof arg == "boolean")) {
                 return { message: `${arg} is not a boolean` };
@@ -186,18 +187,56 @@ const primitives: Record<string, Function> = {
     },
 
     // string comparisons
-    equals: (a: Atom, b: Atom) => stringFun(a, b, (a, b) => a == b),
-    contains: (a: Atom, b: Atom) => stringFun(a, b, (a, b) => a.includes(b)),
-    matches: (a: Atom, b: Atom) => stringFun(a, b, (a, b) => new RegExp(b).test(a)),
+    equals: (args: Atom[]) => stringFun(args[0], args[1], (a, b) => a == b),
+    contains: (args: Atom[]) => stringFun(args[0], args[1], (a, b) => a.includes(b)),
+    matches: (args: Atom[]) => stringFun(args[0], args[1], (a, b) => new RegExp(b).test(a)),
 
     // number comparisons
-    "==": (a: Atom, b: Atom) => numberFun(a, b, (a, b) => a == b),
-    "!=": (a: Atom, b: Atom) => numberFun(a, b, (a, b) => a != b),
-    "<": (a: Atom, b: Atom) => numberFun(a, b, (a, b) => a < b),
-    "<=": (a: Atom, b: Atom) => numberFun(a, b, (a, b) => a <= b),
-    ">": (a: Atom, b: Atom) => numberFun(a, b, (a, b) => a > b),
-    ">=": (a: Atom, b: Atom) => numberFun(a, b, (a, b) => a >= b),
+    "==": (args: Atom[]) => numberFun(args[0], args[1], (a, b) => a == b),
+    "!=": (args: Atom[]) => numberFun(args[0], args[1], (a, b) => a != b),
+    "<": (args: Atom[]) => numberFun(args[0], args[1], (a, b) => a < b),
+    "<=": (args: Atom[]) => numberFun(args[0], args[1], (a, b) => a <= b),
+    ">": (args: Atom[]) => numberFun(args[0], args[1], (a, b) => a > b),
+    ">=": (args: Atom[]) => numberFun(args[0], args[1], (a, b) => a >= b),
+
+    // custom attribute getters
+    "number-attribute": (args: Atom[], env: Environment) => getInEnvNumber(args[0], env),
+    "string-attribute": (args: Atom[], env: Environment) => getInEnvString(args[0], env),
+    "bool-attribute": (args: Atom[], env: Environment) => getInEnvBool(args[0], env),
 };
+
+function getInEnvNumber(name: Atom, env: Environment): number | AudienceError {
+    if (typeof name != "string") {
+        return { message: "can only look up string names" };
+    }
+    const val = env.attributes[name];
+    if (typeof val != "number") {
+        return { message: `'${name}' is not a number` };
+    }
+    return val;
+}
+
+function getInEnvString(name: Atom, env: Environment): string | AudienceError {
+    if (typeof name != "string") {
+        return { message: "can only look up string names" };
+    }
+    const val = env.attributes[name];
+    if (typeof val != "string") {
+        return { message: `'${name}' is not a string` };
+    }
+    return val;
+}
+
+function getInEnvBool(name: Atom, env: Environment): boolean | AudienceError {
+    if (typeof name != "string") {
+        return { message: "can only look up string names" };
+    }
+    const val = env.attributes[name];
+    if (typeof val != "boolean") {
+        return { message: `'${name}' is not a boolean` };
+    }
+    return val;
+}
 
 function isError(x: Atom | AudienceError): x is AudienceError {
     return (x as AudienceError).message !== undefined;
