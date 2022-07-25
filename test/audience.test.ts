8877455e9d912b617f846f7e0c5bdc8306852e32
fabian.bergstrom <fabian.bergstrom@interactivesolutions.se>
2022-07-25 17:28:59 +0200

    make audience tests data driven

--- test/audience.test.ts
+++ test/audience.test.ts
@@ -1,155 +1,100 @@
-import { Atom, Audience, AudienceError } from "../src/audience";
+import fs from "fs";
 
-function evalAudience(json: string, attributes: Record<string, Atom>): boolean | AudienceError {
-    return new Audience(json).eval({ attributes });
-}
-
-function error(message: string): AudienceError {
-    return { message };
-}
-
-function testIs(
-    exprJSON: string,
-    expected: Atom | AudienceError,
-    attributes: Record<string, Atom> = {},
-) {
-    let attrDesc = "";
-    if (0 < Object.keys(attributes).length) {
-        attrDesc = ` with ${JSON.stringify(attributes)}`;
-    }
-    const desc = `${exprJSON}${attrDesc} should be ${JSON.stringify(expected)}`;
-    test(desc, () => {
-        expect(evalAudience(exprJSON, attributes)).toStrictEqual(expected);
-    });
-}
+import { Audience } from "../src/audience";
 
 describe("Audience validation", () => {
-    expect(() => new Audience("[")).toThrow("rules syntax error");
-    expect(() => new Audience("false")).toThrow("AST root must be a list");
-    expect(() => new Audience('["goober"]')).toThrow("goober is not a primitive");
-    expect(() => new Audience("[true]")).toThrow("can only apply strings");
+    const testData = JSON.parse(fs.readFileSync("test/audience_validation_spec.json").toString());
+
+    for (const s of testData) {
+        if (s.skip) {
+            console.log(`skipping test '${s.suite_name}' (because ${s.skip})`);
+            continue;
+        }
+
+        describe(s.suite_name, () => {
+            for (const i in s.test_cases) {
+                const t = s.test_cases[i];
+                const desc = `expression JSON '${t.audience_string}' should error with '${t.expect_error}'`;
+                test(desc, () => {
+                    expect(() => new Audience(t.audience_string)).toThrow(t.expect_error);
+                });
+            }
+        });
+    }
 });
 
-describe("Audience string primitives", () => {
-    testIs('["equals", "", ""]', true);
-    testIs('["equals", "foo", ""]', false);
-    testIs('["equals", "", "bar"]', false);
-    testIs('["equals", "foo", "foo"]', true);
-    testIs('["equals", "foo", "bar"]', false);
+/**
+ * These test cases are defined in JSON to enable portable repeatable testing for multiple SDKs.
+ */
+describe("Audience expression compatibility tests", () => {
+    const testData = JSON.parse(fs.readFileSync("test/audience_spec.json").toString());
 
-    testIs('["contains", "foo", ""]', true);
-    testIs('["contains", "", "foo"]', false);
-    testIs('["contains", "foo", "o"]', true);
-    testIs('["contains", "foo", "a"]', false);
+    for (const s of testData) {
+        if (s.skip) {
+            console.log(`skipping test '${s.suite_name}' (because ${s.skip})`);
+            continue;
+        }
 
-    testIs('["matches", "foo", ""]', true);
-    testIs('["matches", "", "foo"]', false);
-    testIs('["matches", "foo", "(bar|foo)"]', true);
-    testIs('["matches", "foo", "aa*"]', false);
+        describe(s.suite_name, () => {
+            for (const i in s.test_cases) {
+                const t = s.test_cases[i];
+                const audienceJSON = JSON.stringify(t.audience_json);
+                const desc = t.expect_error
+                    ? `expression ${audienceJSON} should error with '${t.expect_error}'`
+                    : `expression ${audienceJSON} should be ${t.expect_result}`;
+                test(desc, () => {
+                    const audience = new Audience(audienceJSON);
+                    const actual = audience.eval({ attributes: {} });
 
-    testIs('["matches", 2, "2"]', error("expected string arguments"));
-    testIs('["equals", "", false]', error("expected string arguments"));
-    testIs('["contains", 4711, 1]', error("expected string arguments"));
+                    if (t.expect_error) {
+                        if (typeof actual === "boolean") {
+                            fail("expected error, got some result");
+                        }
+                        expect(actual.message).toBe(t.expect_error);
+                    } else {
+                        expect(actual).toBe(t.expect_result);
+                    }
+                });
+            }
+        });
+    }
 });
 
-describe("Audience number primitives", () => {
-    testIs('["==", 4711, 1337]', false);
-    testIs('["==", 1337, 1337]', true);
-    testIs('["==", 42, 1337]', false);
+/**
+ * These test cases are defined in JSON to enable portable repeatable testing for multiple SDKs.
+ */
+describe("Audience attributes compatibility tests", () => {
+    const testData = JSON.parse(fs.readFileSync("test/audience_attributes_spec.json").toString());
 
-    testIs('["<", 4711, 1337]', false);
-    testIs('["<", 1337, 1337]', false);
-    testIs('["<", 42, 1337]', true);
+    for (const s of testData) {
+        if (s.skip) {
+            console.log(`skipping test '${s.suite_name}' (because ${s.skip})`);
+            continue;
+        }
 
-    testIs('["<=", 4711, 1337]', false);
-    testIs('["<=", 1337, 1337]', true);
-    testIs('["<=", 42, 1337]', true);
+        describe(s.suite_name, () => {
+            const audienceJSON = JSON.stringify(s.audience_json);
+            const audience = new Audience(audienceJSON);
 
-    testIs('[">", 4711, 1337]', true);
-    testIs('[">", 1337, 1337]', false);
-    testIs('[">", 42, 1337]', false);
+            for (const i in s.test_cases) {
+                const t = s.test_cases[i];
+                const attributes = t.attributes;
+                const desc = t.expect_error
+                    ? `attributes ${JSON.stringify(attributes)} should give err '${t.expect_error}'`
+                    : `attributes ${JSON.stringify(attributes)} should give ${t.expect_result}`;
+                test(desc, () => {
+                    const actual = audience.eval({ attributes });
 
-    testIs('[">=", 4711, 1337]', true);
-    testIs('[">=", 1337, 1337]', true);
-    testIs('[">=", 42, 1337]', false);
-
-    testIs('["==", 2, "2"]', error("expected number arguments"));
-    testIs('["<", "", false]', error("expected number arguments"));
-    testIs('["<=", 1, false]', error("expected number arguments"));
-    testIs('[">", "fo", "ba"]', error("expected number arguments"));
-    testIs('[">=", true, 1]', error("expected number arguments"));
-});
-
-describe("Audience boolean primitives", () => {
-    testIs('["all", true]', true);
-    testIs('["all", true, true, true]', true);
-    testIs('["all"]', true);
-    testIs('["all", false, true, true]', false);
-    testIs('["all", true, false, true]', false);
-    testIs('["all", true, true, false]', false);
-
-    testIs('["any", true, true, true]', true);
-    testIs('["any", true, false, false]', true);
-    testIs('["any", false, true, false]', true);
-    testIs('["any", false, false, true]', true);
-
-    testIs('["any", false, false, false]', false);
-    testIs('["any", false]', false);
-    testIs('["any"]', false);
-
-    testIs('["not", false]', true);
-    testIs('["not", true]', false);
-    testIs('["all", 2, "2"]', error("2 is not a boolean"));
-    testIs('["any", false, "foo"]', error("foo is not a boolean"));
-    testIs('["not", 1]', error("1 is not a boolean"));
-});
-
-describe("Audience nested expressions", () => {
-    testIs('["all", ["all"]]', true);
-    testIs('["all", ["any"]]', false);
-    testIs('["any", ["any"], ["any"], ["all"]]', true);
-    testIs('["any", ["any", ["any", ["all"]]]]', true);
-
-    testIs('["any", ["not", true], ["equals", "foo", "bar"], ["==", 1, 1]]', true);
-    testIs('["any", ["not", false], ["equals", "foo", "bar"], ["==", 1, 2]]', true);
-    testIs('["any", ["not", true], ["equals", "foo", "foo"], ["==", 1, 2]]', true);
-});
-
-describe("Audience docs example expressions", () => {
-    const allExpr = `
-        ["all",
-          [">=", ["number-attribute", "bonusPoints"], 1000],
-          ["contains", ["string-attribute", "urlPath"], "contact"],
-          ["equals", ["string-attribute", "country"], "Sweden"]]`;
-    const anyExpr = `
-        ["any",
-          ["bool-attribute", "preview"],
-          ["equals", ["string-attribute", "environment"], "staging"]]`;
-
-    testIs(allExpr, true, { bonusPoints: 1000, urlPath: "/contact", country: "Sweden" });
-    testIs(allExpr, false, { bonusPoints: 999, urlPath: "/contact", country: "Sweden" });
-    testIs(allExpr, false, { bonusPoints: 1000, urlPath: "/contact", country: "Norway" });
-    testIs(allExpr, false, { bonusPoints: 1000, urlPath: "/home", country: "Sweden" });
-
-    testIs(allExpr, error("'bonusPoints' is not a number"), {
-        urlPath: "/home",
-        country: "Sweden",
-    });
-    testIs(allExpr, error("'urlPath' is not a string"), {
-        bonusPoints: 0,
-        country: "xyz",
-    });
-    testIs(allExpr, error("'country' is not a string"), {
-        bonusPoints: 0,
-        urlPath: "/home",
-    });
-
-    testIs(anyExpr, true, { preview: true, environment: "production" });
-    testIs(anyExpr, true, { preview: false, environment: "staging" });
-    testIs(anyExpr, error("'preview' is not a boolean"), {
-        environment: "staging",
-    });
-    testIs(anyExpr, error("'environment' is not a string"), {
-        preview: true,
-    });
+                    if (t.expect_error) {
+                        if (typeof actual === "boolean") {
+                            fail("expected error, got some result");
+                        }
+                        expect(actual.message).toBe(t.expect_error);
+                    } else {
+                        expect(actual).toBe(t.expect_result);
+                    }
+                });
+            }
+        });
+    }
 });
